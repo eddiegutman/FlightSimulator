f2494270fdc666a5c285655b2452a8df3dc47953
eddiegutman <edigutman@gmail.com>
2021-03-31 10:28:27 -0700

    Added Program.cs and Settings.cs

---
+++ Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1 {
+    class Settings {
+        private string[] attributes;
+
+        public Settings(string path) {
+            XDocument xdoc = XDocument.Load(path);
+            attributes = xdoc.Descendants("output").Descendants("name").Select(e => e.Value).ToArray();
+        }
+
+        public int getIndex(string s) {
+            return Array.IndexOf(attributes, s);
+        }
+    }
+}
